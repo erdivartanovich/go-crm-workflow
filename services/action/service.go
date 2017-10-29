087a75f17924c2a2e70195e2fb230c38dfcfcddf
erdivartanovich <erdivartanovich@gmail.com>
2017-10-29 13:01:38 +0700

    add Action Service

--- services/action/service.go
+++ services/action/service.go
@@ -1,4 +1,42 @@
 package action
 
 type ActionService struct {
+	Repo *ActionRepository
+}
+
+type SearchAdapter struct {
+}
+
+func (service *ActionService) Browse(adapter SearchAdapter) ([]*Action, error) {
+	return service.Repo.SetAdapter(adapter).Find()
+}
+
+func (service *ActionService) Read(action Action) (*Action, error) {
+	return service.Repo.Where(action).First()
+}
+
+func (service *ActionService) Edit(action Action, payload Action) (*Action, error) {
+	a, err := service.Read(action)
+	if err != nil {
+		return nil, err
+	}
+	return service.Repo.Update(*a, payload)
+}
+
+func (service *ActionService) Add(action Action) (*Action, error) {
+	return service.Repo.Insert(action)
+}
+
+func (service *ActionService) Delete(action Action) (*Action, error) {
+	return service.Repo.Delete(action)
+}
+
+func (service *ActionService) Count(adapter SearchAdapter) (int, error) {
+	return service.Repo.SetAdapter(adapter).Count()
+}
+
+func NewActionService() *ActionService {
+	return &ActionService{
+		Repo:NewActionRepository(),
+	}
 }
