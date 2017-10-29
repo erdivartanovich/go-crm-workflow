6685de8b591b3029dce2e702f2f3dfc13c26d7e9
erdivartanovich <erdivartanovich@gmail.com>
2017-10-29 14:39:43 +0700

    add replace method and modify edit method

--- services/action/service.go
+++ services/action/service.go
@@ -1,5 +1,9 @@
 package action
 
+import (
+	"fmt"
+)
+
 type ActionService struct {
 	Repo *ActionRepository
 }
@@ -16,11 +20,46 @@ func (service *ActionService) Read(action Action) (*Action, error) {
 }
 
 func (service *ActionService) Edit(action Action, payload Action) (*Action, error) {
-	return service.Repo.Update(action, payload)
+	wk, err := service.Read(action)
+	if err != nil {
+		return nil, err
+	}
+	return service.Repo.Update(*wk, payload)
+}
+
+func (service *ActionService) Replace(action Action, payload Action) (*Action, error) {
+	return service.Repo.Replace(action, payload)
 }
 
 func (service *ActionService) Add(action Action) (*Action, error) {
 	return service.Repo.Insert(action)
+}
+
+func (service *ActionService) BatchAdd(payloads []Action) (int, error) {
+	var ch chan bool
+	ch = make(chan bool)
+
+	go func() {
+		for i := range payloads {
+			fmt.Println("Lets save", i)
+			_, err := service.Add(payloads[i])
+			if err != nil {
+				ch <- false
+				continue
+			}
+			ch <- true
+		}
+		fmt.Println("closed")
+		close(ch)
+	}()
+	success := 0
+	for n := range ch {
+		if n == true {
+			success++
+		}
+	}
+
+	return success, nil
 }
 
 func (service *ActionService) Delete(action Action) (*Action, error) {
