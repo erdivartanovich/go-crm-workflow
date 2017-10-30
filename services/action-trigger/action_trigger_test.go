0427e05a7519f2f289c8a94d15b79176ce6d73d9
erdivartanovich <erdivartanovich@gmail.com>
2017-10-30 14:47:30 +0700

    add Browse and Add Action  Trigger test

--- services/action-trigger/action_trigger_test.go
+++ services/action-trigger/action_trigger_test.go
@@ -34,3 +34,23 @@ func TestMain(m *testing.M) {
 	shutdown()
 	os.Exit(code)
 }
+
+func TestBrowse(t *testing.T) {
+	a := SearchAdapter{}
+	result, err := service.Browse(a)
+	assert.Nil(t, err, "Error is nil")
+	assert.NotEqual(t, result, "Data should not empty")
+}
+
+func TestAddActionTrigger(t, *testing.T) {
+	model := ActionTrigger{
+		UserID: 1,
+		Name: "test",
+	}
+
+	result, err := service.Add(model)
+
+	assert.Nil(t, err, "Error is nil")
+	assert.Equal(t, mode.Name, result.Name, "It should be equal")
+	assert.NotEqual(t, model.ID, result.Id, "Result ID should not be equal")
+}
