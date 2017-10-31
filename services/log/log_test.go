9ae1599447c5d7f0b8385bae7872ed94d1d2792c
erdivartanovich <erdivartanovich@gmail.com>
2017-10-31 13:48:43 +0700

    add teardown, seed, main test and browse log service test

--- services/log/log_test.go
+++ services/log/log_test.go
@@ -1,10 +1,10 @@
 package log
 
 import (
-	"fmt"
 	"os"
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	"testing"
+	"github.com/stretchr/testify/assert"
 	"github.com/kwri/go-workflow/modules/db"
 	"github.com/kwri/go-workflow/modules/setting"
 	"github.com/kwri/go-workflow/services/entity"
@@ -23,4 +23,36 @@ func setup() {
 	tx := service.Repo.db.Begin()
 	service.Repo.db = tx
 	seedTestData()
+}
+
+func shutdown() {
+	service.Repo.db.Rollback()
+	db.Engine.Close()
+}
+
+func seedTestData() {
+	fmt.Println("testtttt")
+	for i := 1; i <= 10; i++ {
+		model := &entity.WorkflowLog{
+			ResourceName: fmt.Sprintf("test-seed-data-%d", i),
+			UserID: 1,
+			Info: fmt.Sprintf("test-seed-data-info-%d", i),
+		}
+		service.Repo.db.Create(model)
+		dbfixtures = append(dbfixtures, model)
+	}
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
+
+func TestBrowse(t *testing.T) {
+	a := SearchAdapter{}
+	result, err := service.Browse(a)
+	assert.Nil(t, err, "Error is nil")
+	assert.NotEmpty(t, result, "Data should not empty")
 }
