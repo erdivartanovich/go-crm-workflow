294fbcee16df32cf04f890f49a112ec49576e540
erdivartanovich <erdivartanovich@gmail.com>
2017-11-01 18:28:24 +0700

    complete the test

--- services/log/log_test.go
+++ services/log/log_test.go
@@ -56,3 +56,59 @@ func TestBrowse(t *testing.T) {
 	assert.Nil(t, err, "Error is nil")
 	assert.NotEmpty(t, result, "Data should not empty")
 }
+
+func TestAddLog(t *testing.T) {
+
+	model := entity.WorkflowLog{
+		ResourceName: "test",
+		UserID: 1,
+		Info: "test info",
+	}
+
+	result, err := service.Add(model)
+
+	assert.Nil(t, err, "Error is nil")
+	assert.Equal(t, model.ResourceName, result.ResourceName, "It should be equal")
+	assert.NotEqual(t, model.ID, result.ID, "Result ID should have new value")
+}
+
+func TestReadLog(t *testing.T) {
+	fixture := dbfixtures[0]
+	model := entity.WorkflowLog{
+		ID: fixture.ID,
+	}
+	result, err := service.Read(model)
+	assert.Nil(t, err, "Error is nil")
+	assert.Equal(t, fixture.Name, result.Name, "It should be equal")
+	assert.Equal(t, fixture.ID, result.ID, "It should be equal")
+}
+
+func TestEditLog(t *testing.T) {
+	fixture := entity.WorkflowLog{
+		ID: dbfixtures[0].ID,
+	}
+
+	model := entity.WorkflowLog{
+		Name: "edited-test-seed-data",
+	}
+
+	result, err := service.Edit(fixture, model)
+	assert.Nil(t, err, "Error is nil")
+	assert.NotEqual(t, fixture.ResourceName, result.ResourceName, "It should not be equal")
+	assert.Equal(t, model.ResourceName, result.ResourceName, "It should be equal")
+	assert.Equal(t, fixture.ID, result.ID, "It should be equal")
+}
+
+func TestDeleteLog(t *testing.T) {
+	fixture := dbfixtures[0]
+	result, err := service.Delete(*fixture)
+	assert.Nil(t, err, "Error is nil")
+	assert.Equal(t, fixture.ID, result.ID, "It should be equal")
+	assert.NotEmpty(t, result.DeletedAt, "Result ID should have new value")
+}
+
+func TestDeleteEmptyIDError(t *testing.T) {
+	fixture := entity.WorkflowLog{}
+	_, err := service.Delete(fixture)
+	assert.NotNil(t, err, "Error is not nil")
+}
