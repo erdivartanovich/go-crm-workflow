46065f1d17a4bf326fe6ebbd6293f7ecdfcfaf89
erdivartanovich <erdivartanovich@gmail.com>
2017-11-08 18:19:44 +0700

    create seeder for actions

--- migrations/1507579635_create_table_actions.go
+++ migrations/1507579635_create_table_actions.go
@@ -11,7 +11,33 @@ var (
 		ID: "1507579635",
 		Migrate: func(tx *gorm.DB) error {
 			err := tx.CreateTable(&entity.Action{}).Error
-			return err
+			if err != nil {
+				return err
+			}
+			seedData := []*entity.Action{
+				{
+					Name: "Action 1",
+					UserID: 1,
+					ActionType: 1,
+					TaskID: "Task1", 
+				},
+				{
+					Name: "Action 2",
+					UserID: 1,
+					ActionType: 2,
+					TaskID: "Task1", 
+				},
+			}
+			db := tx.Begin()
+			for _, data := range seedData {
+				err = db.Create(data).Error
+				if err != nil {
+					db.Rollback()
+					return err
+				}
+			}
+			db.Commit()
+			return nil
 		},
 		Rollback: func(tx *gorm.DB) error {
 			err := tx.DropTableIfExists(&entity.Action{}).Error
