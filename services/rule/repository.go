364a6f577a7c5bcfea0704e3fc0cbf2339a61c1b
Nurcahyo al Hidayah <2light.hidayah@gmail.com>
2017-11-09 18:28:42 +0700

    Update service rule

--- services/rule/repository.go
+++ services/rule/repository.go
@@ -9,14 +9,18 @@ import (
 	"github.com/kwri/go-workflow/services/entity"
 )
 
+var IncludeMap = map[string]string{
+	"actions": "Actions",
+}
+
 type RuleRepository struct {
 	db      *gorm.DB
 	adapter *entity.SearchAdapter
 	where   *stack.Stack
 }
 
-func (repo *RuleRepository) SetAdapter(adapter entity.SearchAdapter) *RuleRepository {
-	repo.adapter = &adapter
+func (repo *RuleRepository) SetAdapter(adapter *entity.SearchAdapter) *RuleRepository {
+	repo.adapter = adapter
 	return repo
 }
 
@@ -43,6 +47,23 @@ func (repo *RuleRepository) Update(rule entity.Rule, payload entity.Rule) (*enti
 	err := repo.prepareDb().Model(&rule).Update(payload).Error
 	repo.ResetInstance()
 	return &rule, err
+}
+
+func (repo *RuleRepository) Replace(rule entity.Rule, payload entity.Rule) (*entity.Rule, error) {
+	rl := &rule
+	db := repo.prepareDb()
+	db.First(rl)
+	rl.UserID = payload.UserID
+	rl.Name = payload.Name
+	rl.RuleType = payload.RuleType
+	rl.FieldName = payload.FieldName
+	rl.Operator = payload.Operator
+	rl.Value = payload.Value
+	rl.Priority = payload.Priority
+	rl.Actions = payload.Actions
+	err := db.Save(rl).Error
+	repo.ResetInstance()
+	return rl, err
 }
 
 func (repo *RuleRepository) Insert(rule entity.Rule) (*entity.Rule, error) {
@@ -72,11 +93,69 @@ func (repo *RuleRepository) prepareDb() *gorm.DB {
 	for i := 0; i < count; i++ {
 		tx = tx.Where(repo.where.Pop())
 	}
+
+	tx = repo.applySearchAdapter(tx)
+
+	return tx
+}
+
+func (repo *RuleRepository) applySearchAdapter(tx *gorm.DB) *gorm.DB {
+	if repo.adapter != nil {
+		tx = repo.applyInclude(tx)
+		tx = repo.applyFilters(tx)
+		tx = repo.applySorter(tx)
+		tx = repo.applyPager(tx)
+	}
+	return tx
+}
+
+func (repo *RuleRepository) applyInclude(tx *gorm.DB) *gorm.DB {
+	if repo.adapter.Include != nil && len(repo.adapter.Include) > 0 {
+
+		for _, resource := range repo.adapter.Include {
+
+			if val, ok := IncludeMap[resource]; ok {
+				tx = tx.Preload(val)
+			}
+		}
+	}
+
+	return tx
+}
+
+func (repo *RuleRepository) applyFilters(tx *gorm.DB) *gorm.DB {
+	return tx
+}
+
+func (repo *RuleRepository) applySorter(tx *gorm.DB) *gorm.DB {
+	if repo.adapter.Sort != "" {
+		tx = tx.Order(string(repo.adapter.Sort))
+	}
+	return tx
+}
+
+func (repo *RuleRepository) applyPager(tx *gorm.DB) *gorm.DB {
+	limit := 10
+	offset := 0
+
+	if repo.adapter.Page != nil {
+		limit = repo.adapter.Page.Limit
+		offset = repo.adapter.Page.Offset
+	}
+
+	tx = tx.Limit(limit).Offset(offset)
 	return tx
 }
 
 func (repo *RuleRepository) ResetInstance() {
 	repo.adapter = nil
+}
+
+func (repo *RuleRepository) Count() (int, error) {
+	count := 0
+	tx := repo.prepareDb().Limit(-1).Offset(-1)
+	err := tx.Model(&entity.Rule{}).Count(&count).Error
+	return count, err
 }
 
 func NewRuleRepository() *RuleRepository {
--- services/rule/service.go
+++ services/rule/service.go
@@ -6,12 +6,7 @@ type RuleService struct {
 	Repo *RuleRepository
 }
 
-type entity.SearchAdapter struct {
-}
-
-// var repo = NewRuleRepository()
-
-func (service *RuleService) Browse(adapter entity.SearchAdapter) ([]*entity.Rule, error) {
+func (service *RuleService) Browse(adapter *entity.SearchAdapter) ([]*entity.Rule, error) {
 	return service.Repo.SetAdapter(adapter).Find()
 }
 
@@ -20,15 +15,53 @@ func (service *RuleService) Read(rule entity.Rule) (*entity.Rule, error) {
 }
 
 func (service *RuleService) Edit(rule entity.Rule, payload entity.Rule) (*entity.Rule, error) {
-	return service.Repo.Update(rule, payload)
+	rl, err := service.Read(rule)
+
+	if err != nil {
+		return nil, err
+	}
+	return service.Repo.Update(*rl, payload)
+}
+
+func (service *RuleService) Replace(rule entity.Rule, playload entity.Rule) (*entity.Rule, error) {
+	return service.Repo.Replace(rule, playload)
 }
 
 func (service *RuleService) Add(rule entity.Rule) (*entity.Rule, error) {
 	return service.Repo.Insert(rule)
 }
 
+func (service *RuleService) BatchAdd(playloads []entity.Rule) (int, error) {
+	var ch chan bool
+	ch = make(chan bool)
+
+	go func() {
+		for i := range playloads {
+			_, err := service.Add(playloads[i])
+			if err != nil {
+				ch <- false
+				continue
+			}
+			ch <- true
+		}
+		close(ch)
+	}()
+	success := 0
+	for n := range ch {
+		if n == true {
+			success++
+		}
+	}
+
+	return success, nil
+}
+
 func (service *RuleService) Delete(rule entity.Rule) (*entity.Rule, error) {
 	return service.Repo.Delete(rule)
+}
+
+func (service *RuleService) Count(adapter *entity.SearchAdapter) (int, error) {
+	return service.Repo.SetAdapter(adapter).Count()
 }
 
 func NewRuleService() *RuleService {
