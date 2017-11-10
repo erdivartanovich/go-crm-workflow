33e997ca20f4ed2fb2a522938033ee25e879f96e
Nurcahyo al Hidayah <2light.hidayah@gmail.com>
2017-11-10 18:20:29 +0700

    Add seed data in migration table rules

--- migrations/1507635905_create_table_rules.go.go
+++ migrations/1507635905_create_table_rules.go.go
@@ -12,7 +12,40 @@ var (
 		Migrate: func(tx *gorm.DB) error {
 			// Write your migration script here
 			err := tx.CreateTable(&entity.Rule{}).Error
-			return err
+			if err != nil {
+				return err
+			}
+			seedData := []*entity.Rule{
+				{
+					Name:      "Greater",
+					RuleType:  1,
+					FieldName: "Greater Than",
+					Operator:  1,
+					Value:     "1",
+					Priority:  1,
+					UserID:    1,
+				},
+				{
+					Name:      "Lower",
+					RuleType:  2,
+					FieldName: "Lower Than",
+					Operator:  2,
+					Value:     "1",
+					Priority:  1,
+					UserID:    1,
+				},
+			}
+			db := tx.Begin()
+			for _, data := range seedData {
+				err = db.Create(data).Error
+				if err != nil {
+					db.Rollback()
+					return err
+				}
+			}
+
+			db.Commit()
+			return nil
 		},
 		Rollback: func(tx *gorm.DB) error {
 			// Write your migration rollback script here
